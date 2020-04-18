f16bb2bd3b898b47ae6e4b045c8c2c9a8548d681
Wadaha <63825388+Jeong-Sanghun@users.noreply.github.com>
2020-04-18 20:31:06 +0900

    cube move test
    f1, f2, f3를 눌러보시면서 큐브가 잘 움직이는지 확인하시면 됩니다. 깃허브로 유니티 프로젝트를 공유했을 때 에러가 일어나는지, 잘 연동은 되는지 확인하려고 커밋합니다

---
+++ UnityTest/Assets/Scripts/GitTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 vector;
+    Vector3 x, y, z;
+    void Start()
+    {
+        vector = new Vector3(0,0,0);
+        x = new Vector3(1, 0, 0);
+        y = new Vector3(0, 1, 0);
+        z = new Vector3(0, 0, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F1))
+        {
+            vector += x;
+            transform.position = vector;
+        }
+        if (Input.GetKey(KeyCode.F2))
+        {
+            vector += y;
+            transform.position = vector;
+        }
+        if (Input.GetKey(KeyCode.F3))
+        {
+            vector += z;
+            transform.position = vector;
+        }
+
+    }
+}
