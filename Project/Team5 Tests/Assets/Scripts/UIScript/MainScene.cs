5e207ee6b39d8e69c6f0b9daad9c2ba46fe3efd6
Kim-hasa <62535495+Kim-hasa@users.noreply.github.com>
2020-07-10 14:06:57 +0900

    0710

---
+++ Project/Team5 Tests/Assets/Scripts/UIScript/MainScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadCoroutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator LoadCoroutine()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("StartScene");
+    }
+}
